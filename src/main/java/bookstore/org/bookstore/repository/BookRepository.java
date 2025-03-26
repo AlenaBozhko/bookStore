80072ee82afa1707af36fb305e91d06a1ed5022e
AlreadyReady1 <91841492+AlreadyReady1@users.noreply.github.com>
2025-03-26 13:18:22 +0300

    Добавлен Репозиторий книг BookRepository

---
+++ src/main/java/bookstore/org/bookstore/repository/BookRepository.java
@@ -0,0 +1,18 @@
+package bookstore.org.bookstore.repository;
+
+import bookstore.org.bookstore.entity.Book;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+/**
+ * Репозиторий книг
+ */
+@Repository
+public interface BookRepository extends JpaRepository<Book, Long> {
+
+    Optional<Book> findById(Long id);
+
+    Optional<Book> findBookByTitle(String title);
+}
